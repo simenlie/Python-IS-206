6e3274324c831f7408658daf6d3d08b51fbd7129
simenlie <simen_lie@hotmail.com>
2013-09-10 23:23:16 +0200

    Lagt til kommentarer(#) til koden

--- ex11.py
+++ ex11.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+# Skriver ut den første print setningen, deretter må brukeren skrive inn noe
 print "Hvor gammel er du?",
 alder = raw_input()
+
+#Etter brukeren har skrevet inn noe og trykt Enter skriver den ut neste print setning
 print "Hvor høy er du?",
 hoyde = raw_input()
+#Samme som over
 print "Hvor mye veier du?",
 vekt = raw_input()
-
+#Skriver ut en tekst som kombinerer alt brukeren har skrevet inn
 print "Så, du er %r år gammel, %r høy og veier %r." % (alder,hoyde,vekt)
