4f8b123eeef9ed9d1619b542ebb9db4784fb5ba9
simenlie <simen_lie@hotmail.com>
2013-09-10 23:09:34 +0200

    First version of exercise 11

---
+++ ex11.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print "Hvor gammel er du?",
+alder = raw_input()
+print "Hvor høy er du?",
+hoyde = raw_input()
+print "Hvor mye veier du?",
+vekt = raw_input()
+
+print "Så, du er %r år gammel, %r høy og veier %r." % (alder,hoyde,vekt)
