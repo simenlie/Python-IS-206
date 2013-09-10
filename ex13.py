8114a48b4d91acd5faf78016cf08ad5c748d3f0e
simenlie <simen_lie@hotmail.com>
2013-09-11 00:37:32 +0200

    Extended ex13 with a while loop

--- ex13.py
+++ ex13.py
@@ -15,3 +15,16 @@ input_user = raw_input()
 
 print "Thanks. The last thing you typed was %r" % input_user 
 
+var = True
+while var == True :
+	new_input = raw_input("Enter Something more? or 'q' to quit :")
+	if(new_input == "q") :
+		var = False
+		print "You leaving me?! I thought we were friends!"
+	else :
+		print "Okei, you entered ", new_input
+	
+	
+
+print "Nice to talk to you! You are more than welcome to run me again"
+
