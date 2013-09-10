27eea67adc2ff8cff33a190833b2ebe357671b76
simenlie <simen_lie@hotmail.com>
2013-09-11 00:11:30 +0200

    Extended with raw_data input

--- ex13.py
+++ ex13.py
@@ -3,10 +3,15 @@ from sys import argv
 
 script, first, second, third = argv
 
+another = "Give me another one?"
+
 print "The script is called:", script
 print "Your first variable is:", first
 print "your secnd variable is:", second
 print "Your third variable is:", third
 
+print "Okei, you have given me three arguments to store in variables. %r" % another,
+input_user = raw_input()
 
+print "Thanks. The last thing you typed was %r" % input_user 
 
