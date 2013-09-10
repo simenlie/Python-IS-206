e95bcb305fd5877982380b4ed6e842849e2e8481
simenlie <simen_lie@hotmail.com>
2013-09-10 23:53:24 +0200

    Excersise 13 added

---
+++ ex13.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "your secnd variable is:", second
+print "Your third variable is:", third
+
+
+
