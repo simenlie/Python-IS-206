42bf8bb797746cc0e615e12bfef343e3f4eb4900
simenlie <simen_lie@hotmail.com>
2013-09-24 20:15:10 +0200

    Added exercise 1-10. Forgot to do this earlier

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+#A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "siable" or comment out a piece of code:
+#print "This Wont run."
+
+print "This will run."
